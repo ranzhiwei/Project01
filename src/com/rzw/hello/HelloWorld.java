1461dad6565a141f765052cfa54021df7cfa906b
ranzhiwei <15175657842@163.com>
2018-12-28 10:28:18 +0800

    test

---
+++ src/com/rzw/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.rzw.hello;
+
+public class HelloWorld {
+
+//    public static final Customer CUST = new Customer();
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
